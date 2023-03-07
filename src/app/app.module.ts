c4de544d7b726cb343302e119ed9afa1674c9134
Andy Bui <andy.bui@kreativmedia.ch>
2023-03-07 11:32:06 +0100

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Page2Component } from './page2/page2.component';
+import { Page1Component } from './page1/page1.component';
+import { Page3Component } from './page3/page3.component';
+import { HomeComponent } from './home/home.component';
+import { ScullyLibModule } from '@scullyio/ng-lib';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page2Component,
+    Page1Component,
+    Page3Component,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ScullyLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,ts}'],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
+  plugins: [require('daisyui'), require('@tailwindcss/line-clamp')],
+}
